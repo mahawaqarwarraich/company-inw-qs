cc83ae3a8e82a12ac07936aad1b88183b959e7ef
Maha <mahawaqarwarraich@gmail.com>
2026-08-21 18:17:55 +0500

    algo completed

--- stringToInt.cpp
+++ stringToInt.cpp
@@ -5,27 +5,33 @@ using namespace std;
 int stringToInt(string charNum) {
     int intNum = 0;
     int tens = 1;
+    int num;
 
-    for (int i = len(charNum); i >= 0; i--) {
-        if (i == charNum.length-1) {
-            num = ascii(charNum[i]) - 48;
+    // 25-30 = -5 = -5-20 = -25
+
+    for (int i = charNum.length()-1; i >= 0; i--) {
+        if (i == charNum.length()-1) {
+            num = charNum[i] - 48; // to find the original digit
             intNum += num;
         } else if (charNum[i] == '-') {
-            intNumSave = -intNum;
-            intNumSave += 5;
-            
+            int temp = intNum - (intNum+5);
+            int temp2 = temp - (intNum-5);
+            intNum = temp2;     
         } else {
-              num = ascii(charNum[i]) - 48;
+              num = charNum[i] - 48; // to find the original digit
               tens *= 10;
               num *= tens;
               intNum += num;
         }
     }
+
+    return intNum;
 }
 
 int main() {
-    string charNum = "1234";
+    string charNum = "0";
     int intNum = stringToInt(charNum);
     cout << intNum;
+
     return 0;
 }
