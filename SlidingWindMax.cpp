f8d0e3a3af281b8ad0b4025d00053688196964ee
Maha <mahawaqarwarraich@gmail.com>
2026-08-26 23:57:36 +0500

    sliding window max started

---
+++ SlidingWindMax.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> findMax(vector<int> array, int k) {
+    
+}
+
+int main() {
+
+    vector<int> arr = {1, 6, 5, 1, 3, 7, 8};
+    int k = 3;
+
+    vector<int> res = findMax(arr, k);
+    return 0;
+}
--- SumToK.cpp
+++ SumToK.cpp
@@ -1,36 +1,60 @@
 #include<iostream>
 #include<vector>
-#include<unordered_map>
+#include<algorithm>
+#include<unordered_set>
 using namespace std;
 // problem
 // given two unsorted arrays of distinct elements. Find pairs(two elements) such that their sum equals k
 
-// Using hashing o(m)
+// Using hashing o(m+n)
 // arr1 m, arr2 n
 void findPairSum(vector<int> arr1, vector<int> arr2, int k) {
     // Put b's elements in a hash set
-    unordered_map<int, int> map;
+    unordered_set<int> s;
+
     for (int i = 0; i < arr2.size(); i++) {
-        map.insert({i,  arr2[i]});
+      s.insert(arr2[i]);
     }
 
     // Traverse a's elements
     for (int i = 0; i < arr1.size(); i++) {
-        if (map.find(k-arr1[i]) != map.end()) {
+        if (s.find(k-arr1[i]) != s.end()) {
             cout << "(" << arr1[i] << "," << k-arr1[i] << ")" << endl;
         }
     }
-
-
 }
 
-// Using sorting and two pointer approach
+// Using sorting and two pointer approach o(mlogm + nlogn)
+void findPairSum2(vector<int> arr1, vector<int> arr2, int k) {
+   int i = 0;
+   int j = arr2.size()-1;
+
+   // Sort arr1 and arr2
+   sort(arr1.begin(), arr1.end());
+   sort(arr2.begin(), arr2.end());
+
+   while (i < arr1.size() && j >= 0) {
+
+    int sum = arr1[i]+arr2[j];
+
+    if (sum < k) {
+        i++;
+    } else if (sum > k) {
+        j--;
+    } else {
+          cout << "(" << arr1[i] << "," << arr2[j] << ")" << endl;
+          i++;
+          j++;
+    }
+   }
+
+}
 
 int main() {
     vector<int> arr1 = {3, 2, 5};
     vector<int> arr2 = {5, 4, 3};
     int k = 6;
 
-    findPairSum(arr1, arr2, k);
+    findPairSum2(arr1, arr2, k);
     return 0;
 }
