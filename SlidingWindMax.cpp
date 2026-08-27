3bb85377839b4921abcbe234d0579d2edc0a96fb
Maha <mahawaqarwarraich@gmail.com>
2026-08-27 21:52:46 +0500

    sliding window maximum completed

--- SlidingWindMax.cpp
+++ SlidingWindMax.cpp
@@ -1,8 +1,37 @@
 #include<iostream>
 #include<vector>
+#include<deque>
 using namespace std;
 
-vector<int> findMax(vector<int> array, int k) {
+vector<int> findMax(vector<int> arr, int k) {
+    vector<int> res = {}; 
+    deque<int> dec = {};
+
+    
+    
+    for (int i = 0; i < arr.size(); i++) { // n
+        // Remove small elements from dec list to maintain decreasing order
+        while (!dec.empty() && arr[dec.back()] < arr[i]) {
+            dec.pop_back();
+        }
+
+        dec.push_back(i);
+
+        // pop indices that are out of the sliding window (move out)
+        while (!dec.empty() && dec.front() <= i-k) {
+            dec.pop_front();
+        }
+
+        // If window is reached extract the max
+        if (i >= k-1) {
+            // put max into the res list
+            int j = dec.front();
+            res.push_back(arr[j]);
+
+        }
+    }
+
+    return res;
     
 }
 
@@ -12,5 +41,9 @@ int main() {
     int k = 3;
 
     vector<int> res = findMax(arr, k);
+
+    for (int i = 0; i < res.size(); i++) {
+        cout << res[i] << " ";   
+    }
     return 0;
 }
